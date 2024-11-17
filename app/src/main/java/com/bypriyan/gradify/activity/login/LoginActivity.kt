36671af6a111ea0d4917fba05fd1dc5e7ee77228
priyanshu <104priyanshu@gmail.com>
2024-11-17 11:16:10 +0530

    signup screen done

--- app/src/main/java/com/bypriyan/gradify/activity/login/LoginActivity.kt
+++ app/src/main/java/com/bypriyan/gradify/activity/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.bypriyan.gradify.activity.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,8 +10,10 @@ import androidx.core.view.WindowInsetsCompat
 import coil3.load
 import coil3.request.crossfade
 import com.bypriyan.gradify.R
+import com.bypriyan.gradify.activity.signup.SignUpActivity
 import com.bypriyan.gradify.databinding.ActivityLoginBinding
 import com.bypriyan.gradify.databinding.ActivityOnBordingScreenBinding
+import com.bypriyan.gradify.utility.AESHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,6 +25,12 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.registerBtn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+
 
     }
 }
